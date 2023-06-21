7c711b2f051415ca049c016e28f28255d8f1340c
PolarisRisingWar <1499695895@qq.com>
2023-06-21 15:24:59 +0800

    一个论文能用了，在测试另一篇论文时出现问题。先备份一下

--- pptp/convert_pdf.py
+++ pptp/convert_pdf.py
@@ -1,27 +1,31 @@
-import fitz,os,re,json,pdfplumber
+import fitz,os,re,json,tabula
 
 ### 需要用户手动定义的部分
-pdf_file_path="whj_code1/github_code2/parse_paper_to_parts/example1_文字可编辑版/基于深度学习的海关虚假贸易案件罚款金额预测_胡鑫.pdf"
-output_folder="whj_code1/github_code2/parse_paper_to_parts/example1_文字可编辑版/extract_result"  #下次更新检验这个文件夹是否存在，现在请存在
+pdf_file_path="whj_code1/github_code2/parse_paper_to_parts/example2_文字可编辑版/基于深度学习的条件式视觉内容生成研究及应用_张江宁.pdf"
+output_folder="whj_code1/github_code2/parse_paper_to_parts/example2_文字可编辑版/extract_result"  #下次更新检验这个文件夹是否存在，现在请存在
 
 
 ### 执行代码部分
-def process_text(text:str,body_titles:list=[]):
+def process_text(text:str,body_titles:list=[],position:str="正文"):
     """将PDF提取的文本转换为正常分段"""
+    if position=="正文":
+        text=re.sub(r'\[\d+\]', '', text)  #删除引用标
     lines = text.split('\n')
     paragraph_segmentations=".。:："
         
     for i in range(len(lines)):
-        if any([lines[i].endswith(paragraph_segmentation+" ") for paragraph_segmentation in paragraph_segmentations])\
-            or any([body_title in lines[i] for body_title in body_titles]):
-            
+        if any([lines[i].endswith(paragraph_segmentation+" ") for paragraph_segmentation in paragraph_segmentations]):
             lines[i]+="\n"
+        if any([body_title in lines[i] for body_title in body_titles]):
+            lines[i]="\n\n"+lines[i]+"\n"
+        if lines[i]=="!":
+            lines[i]=""
     return ''.join(lines)
 
-def extract_id(description:str):
+def extract_id(description:str,type:str="图"):
     """从图片描述文本中提取ID"""
     line_id=description.split()[0]
-    if line_id=="图":
+    if line_id==type:
         return ' '.join(description.split()[:2])
     else:
         return line_id
@@ -34,7 +38,7 @@ filename_with_extension=os.path.basename(pdf_file_path)
 filename, _ = os.path.splitext(filename_with_extension)
 output_text_file=open(os.path.join(output_folder,filename+"_text.txt"),"w")
 output_image={}
-output_table_file=open(os.path.join(output_folder,filename+"_table.json"),"w")
+output_table={}
 
 image_output_folder=os.path.join(output_folder,filename+"_pic")
 if not os.path.exists(image_output_folder):
@@ -43,6 +47,9 @@ if not os.path.exists(image_output_folder):
 bio_status=0  #0前言，1摘要，2目录，3正文，4参考文献，5附录
 body_titles=[]
 
+zancun_id=set()
+zancunlujing_list=[]
+if_zancunlujing=False
 for page_index in range(len(doc)):
     page=doc[page_index]
 
@@ -85,44 +92,89 @@ for page_index in range(len(doc)):
                     body_titles.append(body_title)
     
     if bio_status==3:
-        output_text_file.write(process_text(texts,body_titles))
+        if "参考文献 " in texts.split("\n"):
+            bio_status=4
+            output_text_file.write("\n\n")
+        else:
+            write_text_total=process_text("\n".join(texts.split("\n")[3:-1]),body_titles)
 
-        #提取图片
-        image_list = page.get_images(full=True)
+            #提取图片
+            image_list = page.get_images(full=True)
 
-        convert_names=[]
-        for image_index, img in enumerate(image_list, start=1): # enumerate the image list
-            y1=page.get_image_bbox(img)[3]  #下沿位置
-            texts2=page.get_text("blocks",sort=True)
+            convert_names=[]
+            for image_index, img in enumerate(image_list, start=1): # enumerate the image list
+                y1=page.get_image_bbox(img)[3]  #下沿位置
+                texts2=page.get_text("blocks",sort=True)
 
-            for line_index in range(len(texts2)):
-                line=texts2[line_index]
-                if int(line[3])==int(y1):  #因为有误差所以不能精确相等
-                    break
+                for line_index in range(len(texts2)):
+                    line=texts2[line_index]
+                    if int(line[3])==int(y1):  #因为有误差所以不能精确相等
+                        break
+                
+                line=texts2[line_index+2]
+                description=line[4].strip()
+                try:
+                    line_id=extract_id(description)
+                except IndexError as e:
+                    line=texts2[line_index+3]
+                    description=line[4].strip()
+                    line_id=extract_id(description)
+                write_text_total=write_text_total.replace(description,"")
+                if not description.startswith("图"):  #子图
+                    if_zancunlujing=True
+                    for lineindex in range(line_index,len(texts2)):
+                        bigger_description=texts2[lineindex][4].strip()
+                        if bigger_description.startswith("图"):
+                            line_id=extract_id(bigger_description)+" "+line_id
+                            description=bigger_description+" "+description
+                            if len(zancunlujing_list)>0:
+                                for zancunlujing in zancunlujing_list:
+                                    dir_name, file_name = os.path.split(zancunlujing)
+                                    new_file_name=extract_id(bigger_description)+" "+file_name
+                                    new_file_path=os.path.join(dir_name,new_file_name)
+                                    os.rename(zancunlujing, new_file_path)
+                            zancunlujing_list=[]
+                            if_zancunlujing=False
+                            break
+                        
+                output_image[line_id]=line[4].strip()
+
+                xref = img[0] # get the XREF of the image
+                pix = fitz.Pixmap(doc, xref) # create a Pixmap
+
+                if pix.n - pix.alpha > 3: # CMYK: convert to RGB first
+                    pix = fitz.Pixmap(fitz.csRGB, pix)
+
+                picture_path=os.path.join(image_output_folder,line_id+".png")
+                if if_zancunlujing:
+                    zancunlujing_list.append(picture_path)
+                pix.save(picture_path,output="png") # save the image as png
+                pix = None
             
-            line=texts2[line_index+2]
-            description=line[4].strip()
-            if not description.startswith("图"):  #子图
-                if texts2[line_index+3][4].strip().startswith("图"):
-                    pass #TODO: 合并图片
+            #提取表格
+            if len(re.findall(r'表 \d+-\d+',texts))>0:
+                for sentence in texts.split("\n"):
+                    if re.match(r'表 \d+-\d+',sentence):
+                        description=sentence
+                        write_text_total=write_text_total.replace(description,"")
+                        table_id=extract_id(description,"表")
+                        output_table[table_id]={"description":description,"value":[]}
+                tables=tabula.read_pdf(pdf_file_path,pages=page_index,multiple_tables=True)
+                if len(tables)>0:
+                    for table in tables:
+                        for index, row in table.iterrows():
+                            output_table[table_id]["value"].append(list(row))
+                else:
+                    pass #TODO: 说明有表格没有提取成功
 
-            line_id=extract_id(description)
+            
 
-            output_image[line_id]=line[4].strip()
+            output_text_file.write(write_text_total)
 
-            xref = img[0] # get the XREF of the image
-            pix = fitz.Pixmap(doc, xref) # create a Pixmap
-
-            if pix.n - pix.alpha > 3: # CMYK: convert to RGB first
-                pix = fitz.Pixmap(fitz.csRGB, pix)
-
-            pix.save(os.path.join(image_output_folder,line_id+".png"),output="png") # save the image as png
-            pix = None
-        
-        #提取表格
-        pdf = pdfplumber.open(pdf_file_path)
-        page = pdf.pages[page_index]
-        if not page.extract_table() is None:
-            pass
+    if bio_status==4:
+        output_text_file.write(process_text("\n".join(texts.split("\n")[3:-1]),body_titles,position="参考文献"))
 
 json.dump(output_image,open(os.path.join(output_folder,filename+"_pic.json"),"w"),ensure_ascii=False)
+json.dump(output_table,open(os.path.join(output_folder,filename+"_table.json"),"w"),ensure_ascii=False)
+
+output_text_file.close()
